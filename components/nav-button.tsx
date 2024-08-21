b46e5f1e97c2e279a9e0f9929db9641366e7b56a
Soyeb Pervez Jim <soyeb.jim@gmail.com>
2024-08-21 09:13:35 +0600

    next step

--- components/nav-button.tsx
+++ components/nav-button.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+
 type Props = {
   href: string;
   label: string;
@@ -5,5 +9,17 @@ type Props = {
 };
 
 export const NavButton = ({ href, label, isActive }: Props) => {
-  return <div>Nav Button</div>;
+  return (
+    <Button
+      asChild
+      size="sm"
+      variant="outline"
+      className={cn(
+        "w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition",
+        isActive ? "bg-white/10 text-white" : "bg-transparent",
+      )}
+    >
+      <Link href={href}>{label}</Link>
+    </Button>
+  );
 };
